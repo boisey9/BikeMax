cb0da5071c212a70d63687dfc95efba878c052f5
Erik Boisvert <erikboisvert@Eriks-MacBook-Pro.local>
2014-08-17 17:26:43 -0400

    first commit

---
+++ SpinningApp/IntensityCollectionViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntensityCollectionViewViewController.h
+//  SpinningApp
+//
+//  Created by Erik Boisvert on 8/1/14.
+//  Copyright (c) 2014 Erik Boisvert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntensityCollectionViewViewController : UICollectionViewController
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SpinningApp
+//
+//  Created by Erik Boisvert on 8/1/14.
+//  Copyright (c) 2014 Erik Boisvert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
